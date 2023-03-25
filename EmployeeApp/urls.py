f006a54041462c976c29a233b0af2448f650d4ab
mnnkhndlwl <khandelwalmanan05@gmail.com>
2023-03-25 21:14:12 +0530

    Baap Commit

---
+++ EmployeeApp/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from EmployeeApp import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns=[
+    path('department/',views.departmentApi),
+    path('department/<int:id>',views.departmentApi),
+    path('employee/',views.employeeApi),
+    path('employee/<int:id>',views.employeeApi),
+    # path(r'^/department/([0-9]+)$',views.departmentApi),
+
+    # path(r'^/employee$',views.employeeApi),
+    # path(r'^/employee/([0-9]+)$',views.employeeApi),
+
+    # path(r'^/employee/savefile',views.SaveFile)
+]
+# +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns = format_suffix_patterns(urlpatterns)
